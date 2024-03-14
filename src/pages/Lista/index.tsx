e337ff58e6bea2e0815d1ff1907c7f38c13728ac
Dayana Mariano <dayanaraquelle@gmail.com>
2024-03-14 17:56:42 -0300

    feat: Card Contato

--- src/pages/Lista/index.tsx
+++ src/pages/Lista/index.tsx
@@ -1,8 +1,9 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { CardContato } from "@/components/CardContato"
 
-const Lista = () => { status
+const Lista = () => {
   
   return (
     <div className="min-h-dvh flex flex-col justify-between text-gray-500">
@@ -19,9 +20,13 @@ const Lista = () => { status
             </Avatar>
           </div>
         </header>
-        <main className="flex mx-14">
-        
-        <h2>Contatos</h2>
+        <main className="flex mx-14 flex-col">
+          <h2 className="flex justify-center text-2xl m-2">Contatos</h2>
+          <CardContato name="Dayana Mariano" tel="(99)99999999" role="Desenvolvedor(a) de Software" photo="https://avatars.githubusercontent.com/u/81884987?v=4" />
+          <CardContato name="Gabryel Machado" tel="(99)99999999" role="Desenvolvedor(a) de Software" photo="https://avatars.githubusercontent.com/u/5809009?v=4"/>
+          <CardContato name="Juliana Mesquita" tel="(99)99999999"  role="Desenvolvedor(a) de Software" photo="https://avatars.githubusercontent.com/u/60270373?v=4"/>
+          <CardContato name="Rodrigo contantino" tel="(99)99999999" role="Desenvolvedor(a) de Software" photo="https://avatars.githubusercontent.com/u/137124681?v=4"/>
+
         </main>
       </div>
       <footer className="flex justify-between m-2 text-xs	">
@@ -29,7 +34,7 @@ const Lista = () => { status
           Feito pelo grupo 2 - Formação Front-end da Ada Tech 2024
         </p>
         <a
-            href="https://github.com/Carlos-Gabryel/contatinhos-ADA/tree/develop"
+            href="https://github.com/Carlos-Gabryel/contatinhos-ADA"
             target="_blank"
           >
             <img
